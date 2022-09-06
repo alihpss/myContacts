62a05cd5c3411f27ac0a1785fc044f43de6a665f
Ali <ali.hps@hotmail.com>
2022-09-05 21:42:22 -0300

    terminando validacao

--- fe/src/components/ContactForm.js/index.js
+++ fe/src/components/ContactForm.js/index.js
@@ -1,46 +1,103 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-import { useState, useRef } from 'react';
-import FormGoup from '../FormGroup';
+import isEmailValid from '../../utils/isEmailValid';
+import formatPhone from '../../utils/formatPhone';
+import useErrors from '../../Hooks/useErrors';
+
 import { Form, ButtonContainer } from './styles';
 
+import FormGoup from '../FormGroup';
 import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
 
 export default function ContacForm({ buttonLabel }) {
   const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
 
-  const email = useRef(null);
+  const {
+    getErrorMessageByFieldName, removeError, setError, errors,
+  } = useErrors();
+
+  const isFormValid = (name && errors.length === 0);
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setError({ field: 'name', message: 'Nome é obrigatório' });
+    } else {
+      removeError('name');
+    }
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      setError({ field: 'email', message: 'E-mail inválido' });
+    } else {
+      removeError('email');
+    }
+  }
+
+  function handlePhoneChange(event) {
+    setPhone(formatPhone(event.target.value));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log({
+      name, email, phone, category,
+    });
+  }
 
   return (
-    <Form>
-      <FormGoup>
+    <Form onSubmit={handleSubmit} noValidate>
+      <FormGoup error={getErrorMessageByFieldName('name')}>
         <Input
-          placeholder="Nome"
+          error={getErrorMessageByFieldName('name')}
+          placeholder="Nome *"
           value={name}
-          onChange={(event) => setName(event.target.value)}
+          onChange={handleNameChange}
         />
       </FormGoup>
 
-      <FormGoup
-        error="O formato do e-mail é inválido"
-      >
-        <Input placeholder="E-mail" ref={email} />
+      <FormGoup error={getErrorMessageByFieldName('email')}>
+        <Input
+          type="email"
+          error={getErrorMessageByFieldName('email')}
+          placeholder="E-mail"
+          value={email}
+          onChange={handleEmailChange}
+        />
       </FormGoup>
 
       <FormGoup>
-        <Input placeholder="Telefone" />
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={handlePhoneChange}
+          maxLength="15"
+        />
       </FormGoup>
 
       <FormGoup>
-        <Select>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="">Categoria</option>
           <option value="instagram">Instagram</option>
+          <option value="instagram">Discord</option>
         </Select>
       </FormGoup>
 
       <ButtonContainer>
-        <Button type="submit">
+        <Button type="submit" disabled={!isFormValid}>
           {buttonLabel}
         </Button>
       </ButtonContainer>
